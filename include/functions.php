3a5a4bcd898deea06a4b11adb2b92296db1097c3
Josh CA <contact.gflet@gmail.com>
2020-08-08 22:41:40 +0100

    xtd

--- include/functions.php
+++
@@ -1,4 +0,0 @@
-<?php 
-##Here I Will Be Defining my functions
-
-?>
--- include/variables.php
+++
@@ -1,7 +0,0 @@
-<?php 
-##Here I Will Be Defining my Variables
-$getUrl = dirname($_SERVER['PHP_SELF']);
-$RootDir = $_SERVER['DOCUMENT_ROOT'];
-
-
-?>
